4947365a01c32c38023e5db8a7261028d57abbbf
Assem <assem.ukg@gmail.com>
2017-09-14 15:58:17 +0600

    first lab

---
+++ Lab1/hw1/hw1/hw1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HW1
+{
+
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			int n, i;
+			Console.Write("Number of enters: ");
+			n = int.Parse(Console.ReadLine());
+			int[] numbers = new int[n];
+			Console.WriteLine("Enter numbers: ");
+			string[] s = Console.ReadLine().Split(' ');
+			for (i = 0; i < s.Length; i++)
+			{
+
+				numbers[i] = int.Parse(s[i]);
+
+				if (isPrime(i))
+					Console.Write(i.ToString() + " ");
+
+			}
+
+		}
+
+
+		public static bool isPrime(int i)
+		{
+			if (i == 1)
+				return false;
+			if (i == 2)
+				return true;
+
+			for (int j = 3; j * j <= i; j++)
+			{
+				if (i % j == 0)
+					return false;
+			}
+			return true;
+
+
+		}
+	}
+}
