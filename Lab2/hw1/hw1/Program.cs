592aa5ecf7709b4269a4ef7766d858eb0b99b983
Assem <assem.ukg@gmail.com>
2017-09-28 14:12:28 +0600

    labs

---
+++ Lab2/hw1/hw1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+		String[] lines = File.ReadAllLines(@"/Users/macbook/Desktop/PTlab/Lab2/WriteText.txt");
+        long max = long.MinValue;
+        long min = long.MaxValue;
+		long score = 0;
+        Console.Write("Input numbers:");
+		foreach (String line in lines)
+		{
+            Console.WriteLine(line);
+
+			if (Int64.TryParse(line, out score))
+			{
+				if (score > max)
+					max = score;
+                if (score < min)
+                    min = score;
+			}
+		}
+		Console.WriteLine("\n" + "Maximum Score is " + max);
+        Console.WriteLine("\n" + "Minimum Score is " + min);
+
+
+
+	}
+
+
+}
---
+++ Lab2/hw2/hw2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		String[] lines = File.ReadAllLines(@"/Users/macbook/Desktop/PTlab/Lab2/WriteText.txt");
+		long score = 0;
+        long min = long.MaxValue;
+        ArrayList arr = new ArrayList ();
+        Console.WriteLine("Input numbers:");
+		foreach (String line in lines)
+		{
+            Console.Write(line.ToString() + " ");
+		}
+        Console.WriteLine("\n" + "Minimum simple number is: ");
+		foreach (String line in lines)
+		{
+			
+
+            if (Int64.TryParse(line, out score))
+			{
+                if (isSimple(score))
+                {
+                    arr.Add(score);
+                    foreach (long scores in arr)
+                    {
+                        if (min > scores)
+                            min = scores;
+                    }
+				}
+			}
+		}
+
+        Console.WriteLine(min);
+		using (StreamWriter file =
+			new StreamWriter(@"/Users/macbook/Desktop/PTlab/Lab2/WriteText2.txt"))
+        {
+            file.WriteLine("\n" + "Minimum simple number is: " + min);
+        }
+	}
+
+	private static bool isSimple(long N)
+	{
+		if (N == 1 || N == 0)
+			return false;
+
+		for (int j = 2; j <= (int)(N / 2); j++)
+		{
+			if (N % j == 0)
+				return false;
+		}
+
+		return true;
+
+	}
+
+
+}
