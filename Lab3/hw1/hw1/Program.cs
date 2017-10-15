ff448dad7308cf50d135d99ca4889b3bcf8152d0
Assem <assem.ukg@gmail.com>
2017-10-15 14:24:17 +0600

    Lab3

---
+++ Lab3/hw1/hw1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace hw1
+{
+	class Program
+	{
+		
+		static void ser()
+		{
+			Student student = new Student("Assem", "Urazalimova" , 19 , "16BD02161" , "FIT");
+			BinaryFormatter binary = new BinaryFormatter();
+			FileStream file = new FileStream("Lab3.xml", FileMode.OpenOrCreate, FileAccess.Write);
+
+			try
+			{
+				binary.Serialize(file, student);
+			}
+			catch 
+			{
+				Console.Write("Error");
+			}
+			finally
+			{
+				file.Close();
+			}
+		}
+
+		static void deser()
+		{
+			BinaryFormatter binary = new BinaryFormatter();
+			FileStream file = new FileStream("Lab3.xml", FileMode.OpenOrCreate, FileAccess.Read);
+			Student student = new Student();
+			try
+			{
+				student = binary.Deserialize(file) as Student;
+			}
+			catch
+			{
+				Console.Write("error");
+			}
+			finally
+			{
+				file.Close();
+			}
+
+			Console.Write(student);
+		}
+
+
+		static void Main(string[] args)
+		{
+            ser();
+			deser();
+		}
+	}
+}
---
+++ Lab3/hw1/hw1/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace hw1
+{
+    [Serializable]
+    public class Student
+
+    {  
+        public string name;
+        public string surname;
+        public int age;
+        public string id;
+        public string faculty;
+           
+
+            public Student() { }
+
+            public Student(string name, string surname, int age, string id, string faculty)
+            {
+            
+            this.name = name;
+            this.surname = surname;
+            this.age = age;
+            this.id = id;
+            this.faculty = faculty;
+
+            }
+
+            public override string ToString()
+            {
+            return "Name: " + name + "\nSurname: " + surname + "\nAge: " + age + "\nStudent ID: " + id + "\nFaculty: " + faculty;
+            }
+
+
+    }
+}
